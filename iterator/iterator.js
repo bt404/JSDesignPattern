47f24cbd3b8ab1a8e4c2ec1324a9602406a36830
bt404 <baitang404@gmail.com>
2016-01-28 18:24:28 +0800

    add iterator.js

---
+++ iterator/iterator.js
@@ -0,0 +1,45 @@
+/*
+ * 外部迭代器
+ */
+
+var Iterator = function (obj) {
+  var current = 0;
+
+  var isDone = function () {
+    return current >= obj.length;
+  };
+
+  var next = function () {
+    current++;
+  };
+
+  var getCurrent = function () {
+    return obj[current];
+  };
+
+  return {
+    isDone: isDone,
+    next: next,
+    getCurrent: getCurrent
+  };
+};
+
+var iterator1 = Iterator([1, 2, 3]);
+var iterator2 = Iterator([1, 3, 3]);
+var iterator3 = Iterator([1, 3, 3]);
+var iterator4 = Iterator([1, 3, 3]);
+
+var compare = function (iteratorA, iteratorB) {
+  while (!iteratorA.isDone() || !iteratorB.isDone()) {
+    if (iteratorA.getCurrent() != iteratorB.getCurrent()) {
+      console.log('not same');
+      return;
+    }
+    iteratorA.next();
+    iteratorB.next();
+  }
+  console.log('same');
+};
+
+compare(iterator1, iterator2);
+compare(iterator3, iterator4);
