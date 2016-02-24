fe5c161f4e9f95fcef8823ec20313c68ac6d909f
bt404 <baitang404@gmail.com>
2016-02-24 16:34:13 +0800

    add 杨辉三角数组/递归实现

--- basic/tranle_array.js
+++ basic/tranle_array.js
@@ -1,17 +1,15 @@
 'use strict';
 
 function trangle (lines) {
-  let result = [[1], [1, 1]];
+  let result = [];
 
-  if (lines >= 3) {
-    for (let i = 2; i < lines; i++) {
-      result[i] = [];
-      for (let j = 0; j <= i; j++) {
-        if (j == 0 || j == i) {
-          result[i].push(1);
-        } else {
-          result[i].push(result[i-1][j-1] + result[i-1][j]);
-        }
+  for (let i = 0; i < lines; i++) {
+    result[i] = [];
+    for (let j = 0; j <= i; j++) {
+      if (j == 0 || j == i) {
+        result[i].push(1);
+      } else {
+        result[i].push(result[i-1][j-1] + result[i-1][j]);
       }
     }
   }
