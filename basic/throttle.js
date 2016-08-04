b184daffa710eddfa0c2aec5b591d0bd337b6cdc
bt404 <baitang404@gmail.com>
2016-08-04 16:36:08 +0800

    throttle example

--- basic/throttle.js
+++ basic/throttle.js
@@ -4,9 +4,10 @@ var throttle = function(fn, interval) {
 
     return function() {
         var __me = this;
+        var __args = arguments;
         
         if (firsttime) {
-            fn.apply(__me, arguments);
+            fn.apply(__me, __args);
             return firsttime = false;
         }
 
@@ -17,10 +18,14 @@ var throttle = function(fn, interval) {
         timer = setTimeout(function () {
             clearTimeout(timer);
             timer = null;
-            fn.apply(__me, arguments);
+            fn.apply(__me, __args);
         }, interval || 1000);
     };
 };
 
 var foo = throttle(function(){console.log(arguments[0]);}, 10);
+
+foo(1);
+foo(2);
+setTimeout(function(){foo(3);foo(4);}, 100);
 
