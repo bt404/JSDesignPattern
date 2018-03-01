f74a59095b948cd2d761586c72fcb4f116f589be
zee <guojingze@qudian.com>
2018-03-01 10:07:59 +0800

    upd: array uniq

--- algorithm/game/array_uniq.js
+++ algorithm/game/array_uniq.js
@@ -2,15 +2,25 @@ function arrayUniq (con) {
   let temp = {},
       ret = [];
   for (let item of con) {
-    let key = JSON.stringify(item);
-    if (!temp[key]) {
-      temp[key] = 1;
+    // 引用类型直接添加
+    if (typeof item === 'object') {
       ret.push(item);
+    } else {
+      let key = JSON.stringify(item);
+      if (!temp[key]) {
+        temp[key] = 1;
+        ret.push(item);
+      }
     }
   }
   return ret;
 }
 
+function arrayUniqBySet (con) {
+  return Array.from(new Set(con));
+}
+
 let con = [1, '1', 1, 2, 3, 'hehe', {a: 1}, {a: 1}];
 
 console.log(arrayUniq(con));
+console.log(arrayUniqBySet(con));
