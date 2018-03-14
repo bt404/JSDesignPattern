3071c4f862d360c25a52c7fca13aaee5b4600bd8
zee <zee@nodefe.com>
2018-03-14 11:56:00 +0800

    upd: realize map with reduce

---
+++ algorithm/library/reduce_map.js
@@ -0,0 +1,12 @@
+Array.prototype.map = function (fn) {
+  let ret = [];
+  this.reduce((prev, cur) => {
+    ret.push(fn.call(null, cur));
+    return cur;
+  }, null);
+  return ret;
+}
+
+let con = [1, 2, 3];
+
+console.log(con.map(item => item * 2));
