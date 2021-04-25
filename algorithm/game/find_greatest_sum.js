de295935b41eccc98ba0b4cf9f251bfbd2c48128
zee <zee@nodefe.com>
2021-04-25 13:40:31 +0800

    opt

--- algorithm/game/find_greatest_sum.js
+++ algorithm/game/find_greatest_sum.js
@@ -1,24 +1,21 @@
 /*
   找到一个数组中最大子数组和
  */
-function findGreatestSum (con) {
-  let temp = [];
-  for (let i = 0; i < con.length; ++i) {
-    temp[i] = con[i];
-  }
-
-  let ret = temp[0];
+function findGreatestSum(con) {
+  let temp, max;
+  temp = max = con[0];
 
   for (let i = 1; i < con.length; ++i) {
-    if (temp[i - 1] > 0) {
-      temp[i] = temp[i - 1] + temp[i];
+    if (temp < 0) {
+      temp = con[i];
+    } else {
+      temp += con[i];
     }
-    if (ret < temp[i]) {
-      ret = temp[i];
-    }
+
+    max = max < temp ? temp : max;
   }
 
-  return ret;
+  return max;
 }
 
 let con = [1, 4, -3, 5, -1, 6, 2, -5];
